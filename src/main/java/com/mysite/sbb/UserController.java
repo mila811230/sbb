f92b0a51c7085de39de2c5bfbdf540dfec29171a
mila811230 <bluegalaxy1230@gmail.com>
2024-10-08 14:39:26 +0900

    getUser()구현

--- src/main/java/com/mysite/sbb/UserController.java
+++ src/main/java/com/mysite/sbb/UserController.java
@@ -1,14 +1,26 @@
 package com.mysite.sbb;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class UserController {
 	
+	@Autowired
+	private UserService userService;
+	
 	@GetMapping("/user/login")
 	public String login() {
 		return "/users/login";
+	}
+	
+	@GetMapping("/user/{username}")
+	@ResponseBody
+	public User getUser(@PathVariable("username") String username) {
+		return userService.getUser();
 	}
 
 }
