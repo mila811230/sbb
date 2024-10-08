f91ace11ccb8751f94e5ebbe0382d46be2df7cd1
mila811230 <bluegalaxy1230@gmail.com>
2024-10-08 17:38:11 +0900

    login메세지

--- src/main/java/com/mysite/sbb/UserController.java
+++ src/main/java/com/mysite/sbb/UserController.java
@@ -2,8 +2,11 @@ package com.mysite.sbb;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -14,13 +17,22 @@ public class UserController {
 	
 	@GetMapping("/user/login")
 	public String login() {
-		return "/users/login";
+		return "/user/login";
 	}
 	
-	@GetMapping("/user/{username}")
-	@ResponseBody
-	public User getUser(@PathVariable("username") String username) {
-		return userService.getUser(username);
+	@PostMapping("/user/login")
+	public void login2(@RequestBody User user) {
+		User newUser = user;
 	}
+	
+	//상세정보 /user/hong
+	@GetMapping("/user/{username}")
+	public String getUser(@PathVariable("username") String username, Model model) {
+		User user = userService.getUser(username);
+		model.addAttribute("user", user);
+		return "/user/detail";
+	}
+	
+	
 
 }
